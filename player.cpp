055d46fbf621002c4fc1db867f65a474876f6cf4
Gahnik <39975019+Gahnik@users.noreply.github.com>
2023-03-25 18:06:39 -0500

    Updated functions dealing with scores

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <iostream>
 #include "player.h"
 using namespace std;
@@ -6,13 +7,15 @@ using namespace std;
 Player::Player(){
 	username = " ";
 	password = " ";
-	score = 0;
+	highScore = 0;
+	scores.push_back(0);
 }
 
 Player::Player(string u, string p, int s){
 	username = u;
 	password = p;
-	score = s;
+	scores.push_back(s);
+	highScore = s;
 }
 
 void Player::setUsername(string u){
@@ -23,8 +26,16 @@ void Player::setPassword(string p){
 	password = p;
 }
 
-void Player::setScore(int s){
-	score = s;
+void Player::addScore(int s){
+	scores.push_back(s);
+}
+
+void Player::updateHighScore(){
+	for(int i = 0; i < scores.size(); i++){
+		if (scores[i] > highScore){
+			highScore = scores[i];
+		}
+	}
 }
 
 string Player::getUsername(){
@@ -35,12 +46,12 @@ string Player::getPassword(){
 	return password;
 }
 
-int Player::getScore(){
-	return score;
+int Player::getHighScore(){
+	return highScore;
 }
 
 void Player::printPlayer(){
 	cout << "Username: " << username << endl;
 	cout << "Password: " << password << endl;
-	cout << "Score: " << score << endl;
+	cout << "High Score: " << highScore << endl;
 }
