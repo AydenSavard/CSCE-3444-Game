c5a942ce3e94d5d620fe1b50e0cb96f6df8e0d12
AydenSavard <126193519+AydenSavard@users.noreply.github.com>
2023-03-25 14:59:39 -0500

    Update boardSetup.cpp

--- boardSetup.cpp
+++ boardSetup.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 #include "boardSetup.h"
 using namespace std;
 
-void boardSetup(string bSize){//creates a board of the given size filled with 'e's
-    vector<vector<char>> board;
-    int rows = bSize.at(0)-48;
+void boardSetup(string bSize, vector<vector<int>> &board){//creates a board of the given size filled with 'e's
+    srand(time(nullptr));
+	
+	int rows = bSize.at(0)-48;
     int coll = bSize.at(2)-48;
     cout << rows << "x" << coll << endl;//outputs board size
     
-    for(int i=0; i<rows;i++){//creating the e board
-        vector<char> empt;
+    for(int i=0; i<rows;i++){//creating the board of 1s and 2s
+        vector<int> empt;
         for(int j=0; j<coll;j++){
-            empt.push_back('e');
+            empt.push_back(rand() % 2 + 1);
         }
         board.push_back(empt);
     }
+	board[rand() % rows][rand() % coll] = 3;	//adding the "player"
+	board[rand() % rows][rand() % coll] = 4;	//adding the "exit"
 
     for(int i=0; i<board.size();i++){//outputs the board, can be removed if not needed
         for(int j=0; j<board[i].size();j++){
